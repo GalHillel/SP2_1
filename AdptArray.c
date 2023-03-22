3e91f0e269183febb6229347d426e76c63f22aff
gal <Galh2011@gmail.com>
2023-03-22 10:38:56 +0200

    final committed

--- AdptArray.c
+++ AdptArray.c
@@ -3,98 +3,117 @@
 #include <string.h>
 #include "AdptArray.h"
 
-typedef struct AdptArray_ {
-    PElement* elements;
+typedef struct AdptArray_
+{
     int size;
-    int capacity;
-    COPY_FUNC copy_func;
-    DEL_FUNC del_func;
-    PRINT_FUNC print_func;
+    PElement *elements;
+    DEL_FUNC deleteFunc;
+    COPY_FUNC copyFunc;
+    PRINT_FUNC printFunc;
 } AdptArray;
 
-PAdptArray CreateAdptArray(COPY_FUNC copy_func, DEL_FUNC del_func, PRINT_FUNC print_func) {
+PAdptArray CreateAdptArray(COPY_FUNC copyFunc, DEL_FUNC deleteFunc, PRINT_FUNC printFunc)
+{
     PAdptArray arr = (PAdptArray)malloc(sizeof(AdptArray));
-    if (!arr) {
+    if (arr == NULL)
+    {
         return NULL;
     }
     arr->size = 0;
-    arr->capacity = 1;
-    arr->copy_func = copy_func;
-    arr->del_func = del_func;
-    arr->print_func = print_func;
-    arr->elements = (PElement*)malloc(sizeof(PElement) * arr->capacity);
-    if (!arr->elements) {
-        free(arr);
-        return NULL;
-    }
+    arr->elements = NULL;
+    arr->deleteFunc = deleteFunc;
+    arr->copyFunc = copyFunc;
+    arr->printFunc = printFunc;
     return arr;
 }
 
-void DeleteAdptArray(PAdptArray arr) {
-    if (!arr) {
+void DeleteAdptArray(PAdptArray arr)
+{
+    if (arr == NULL)
+    {
         return;
     }
-    for (int i = 0; i < arr->size; i++) {
-        if (arr->elements[i]) {
-            arr->del_func(arr->elements[i]);
+    for (int i = 0; i < arr->size; ++i)
+    {
+        if (arr->elements[i] != NULL)
+        {
+            arr->deleteFunc(arr->elements[i]);
         }
     }
     free(arr->elements);
     free(arr);
 }
 
-Result SetAdptArrayAt(PAdptArray arr, int index, PElement element) {
-    if (!arr || index < 0) {
+Result SetAdptArrayAt(PAdptArray arr, int index, PElement newElement)
+{
+    if (arr == NULL || newElement == NULL)
+    {
         return FAIL;
     }
-    if (index >= arr->capacity) {
-        int new_capacity = index + 1;
-        PElement* new_elements = (PElement*)realloc(arr->elements, sizeof(PElement) * new_capacity);
-        if (!new_elements) {
+    PElement *newElements;
+    if (index >= arr->size)
+    {
+        newElements = (PElement *)calloc((index + 1), sizeof(PElement));
+        if (newElements == NULL)
+        {
             return FAIL;
         }
-        memset(new_elements + arr->capacity, 0, sizeof(PElement) * (new_capacity - arr->capacity));
-        arr->elements = new_elements;
-        arr->capacity = new_capacity;
+        for (int i = 0; i < arr->size; i++)
+        {
+            if (arr->elements[i] != NULL)
+            {
+                newElements[i] = arr->elements[i];
+            }
+        }
+        free(arr->elements);
+        arr->elements = newElements;
     }
-    if (arr->elements[index]) {
-        arr->del_func(arr->elements[index]);
+    if (arr->elements[index] != NULL)
+    {
+        arr->deleteFunc(arr->elements[index]);
     }
-    arr->elements[index] = arr->copy_func(element);
-    if (!arr->elements[index]) {
-        return FAIL;
-    }
-    if (index >= arr->size) {
-        arr->size = index + 1;
-    }
+    arr->elements[index] = arr->copyFunc(newElement);
+    arr->size = (index >= arr->size) ? (index + 1) : arr->size;
     return SUCCESS;
 }
 
-PElement GetAdptArrayAt(PAdptArray arr, int index) {
-    if (!arr || index < 0 || index >= arr->size) {
+PElement GetAdptArrayAt(PAdptArray arr, int index)
+{
+    if (arr == NULL || index < 0 || index >= arr->size)
+    {
         return NULL;
     }
-    return arr->copy_func(arr->elements[index]);
+    if (arr->elements[index] != NULL)
+    {
+        return arr->copyFunc(arr->elements[index]);
+    }
+    return NULL;
 }
 
-int GetAdptArraySize(PAdptArray arr) {
-    if (!arr) {
+int GetAdptArraySize(PAdptArray arr)
+{
+    if (arr == NULL)
+    {
         return -1;
     }
     return arr->size;
 }
 
-void PrintDB(PAdptArray arr) {
-    if (!arr) {
+void PrintDB(PAdptArray arr)
+{
+    if (arr == NULL)
+    {
+        printf("Invalid array\n");
         return;
     }
-    printf("Adaptive Array:\n");
-    for (int i = 0; i < arr->size; i++) {
-        printf("[%d]: ", i);
-        if (arr->elements[i]) {
-            arr->print_func(arr->elements[i]);
-        } else {
-            printf("NULL\n");
+
+    for (int i = 0; i < arr->size; i++)
+    {
+        if (arr->elements[i] != NULL)
+        {
+            printf("Element %d:\n", i);
+            arr->printFunc(arr->elements[i]);
+            printf("\n");
         }
     }
 }
